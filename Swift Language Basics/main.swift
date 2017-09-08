86556236c521aefe23ac86f7c0cd6cc646174b6f
AndrewStoll <aws52b@mail.missouri.edu>
2017-09-08 14:08:12 -0500

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Andrew W Stoll on 9/8/17.
+//  Copyright © 2017 Andrew W Stoll. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
